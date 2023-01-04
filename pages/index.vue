400fdc11e1ddc098f1db5d67c8165ac5182f63d9
laurent <laurentpcaron@gmail.com>
2023-01-03 21:11:35 -0500

    First one

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>Laurent-Philippe Caron</h2>
+  <p class="text-sm">Developpeur front-end</p>
+  <img src="~/assets/img/profil.png" alt="Portrait de Laurent-Philippe Caron" />
+  <div>
+    <ul>
+      <li><strong>Nom : </strong>Laurent</li>
+      <li><strong>Employeur</strong></li>
+      <li><strong>Email</strong></li>
+      <li><strong></strong></li>
+      <li><strong></strong></li>
+      <li><strong></strong></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
