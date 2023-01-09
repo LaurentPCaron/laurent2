811fd6c2c5df7f1154b2ad94cc82cbf30f1e7a26
laurent <laurentpcaron@gmail.com>
2023-01-08 22:38:43 -0500

    Ajout font awsome

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,5 @@
 <template>
-  <h2>Laurent-Philippe Caron</h2>
-  <p class="text-sm">Developpeur front-end</p>
-  <img src="~/assets/img/profil.png" alt="Portrait de Laurent-Philippe Caron" />
-  <div>
-    <ul>
-      <li><strong>Nom : </strong>Laurent</li>
-      <li><strong>Employeur</strong></li>
-      <li><strong>Email</strong></li>
-      <li><strong></strong></li>
-      <li><strong></strong></li>
-      <li><strong></strong></li>
-    </ul>
-  </div>
+  <font-awesome-icon icon="fa-regular fa-lemon" />
 </template>
 
 <script>
