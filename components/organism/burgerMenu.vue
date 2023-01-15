fda7177fbe1a811b6ce6b6cd31129e9773d7cdaa
laurent <laurentpcaron@gmail.com>
2023-01-14 22:52:15 -0500

    Mains start

--- components/organism/burgerMenu.vue
+++ components/organism/burgerMenu.vue
@@ -3,16 +3,16 @@
     <font-awesome-icon icon="fa-solid fa-bars" class="text-4xl text-white" />
   </button>
   <div
-    class="absolute top-0 left-0 w-full h-screen transition-colors duration-700"
+    class="absolute top-0 right-0 w-full h-screen transition-colors duration-700"
     :class="[{ 'bg-black/50': isOpen }, backgroundState]"
     @[isOpen&&`click`]="togglingMenu"
   ></div>
   <nav
-    class="bg-black text-white fixed top-20 left-0 h-full w-1/4 transition-all duration-700"
-    :class="isOpen ? 'translate-x-0' : '-translate-x-full'"
+    class="flex justify-center bg-black text-white fixed top-20 right-0 h-full w-1/4 transition-all duration-700"
+    :class="isOpen ? 'translate-x-0' : 'translate-x-full'"
   >
-    <ul class="ml-5" :class="menuItemsState">
-      <li>
+    <ul class="text-right" :class="menuItemsState">
+      <li class="mb-10">
         <button @click="togglingMenu">
           <font-awesome-icon icon="fa-solid fa-xmark" />
         </button>
--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,13 @@
     <font-awesome-icon icon="fa-solid fa-leaf" class="text-6xl text-white" />
     <OrganismBurgerMenu />
   </header>
-  <div class="flex justify-center pt-16 mb-24 bg-neutral-700">
-    <img
-      class="w-1/5 -mb-24 rounded-full border-white border-8"
-      src="~/assets/img/profil.png"
-      alt=""
-    />
+  <div class="h-full flex flex-1">
+    <div class="bg-pink-50 w-6/12"></div>
+    <div class="flex items-center w-5/12">
+      <img class="" src="~/assets/img/Eggman_02.png" alt="" />
+    </div>
+    <div class="bg-green-700 w-1/12"></div>
   </div>
-  <h1 class="text-center">MON SITE WEB</h1>
 </template>
 
 <script>
