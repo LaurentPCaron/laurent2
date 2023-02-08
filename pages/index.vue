e90194fc6ed18461ab2eabf27469e079580a2ced
laurent <laurentpcaron@gmail.com>
2023-02-08 18:00:33 -0500

    Debut pour progress

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,11 @@
     <OrganismBurgerMenu class="md:hidden md:mt-0 mr-5" />
   </header>
   <OrganismHomePart class="pt-6" />
+
+  <div class="">
+    <div class="relative top-0 w-56 h-56 rotate-45 bg-transparent z-10"></div>
+    <div class="relative top-0 w-56 h-56 bg-light rounded-full"></div>
+  </div>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
       dark: '#3C2A21',
       light: '#D5CEA3',
       lighter: '#E5E5CB',
+      inherit: 'inherit',
+      transparent: 'transparent',
     },
   },
 };
