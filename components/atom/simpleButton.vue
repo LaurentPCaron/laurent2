85a187f73f9ef4df6e023c64dc6d82fd63739d2e
LaurentPCaron <laurentpcaron@gmail.com>
2023-05-21 22:38:26 -0400

    .

--- components/atom/simpleButton.vue
+++ components/atom/simpleButton.vue
@@ -1,9 +1,13 @@
 <template>
   <button
-    class="bg-dark hover:bg-lighter text-light hover:text-mains font-semibold w-fit px-12 py-3"
+    class="bg-light hover:bg-lighter text-light hover:text-mains border-2 shadow-main border-lighter font-semibold w-fit px-12 py-3 button"
   >
     <slot />
   </button>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.button {
+  box-shadow: 0 10px 1px #000;
+}
+</style>
