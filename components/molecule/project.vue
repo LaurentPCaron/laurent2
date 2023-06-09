6881a7ee759f46ce27b597b07e27cc727d8df90a
laurent <laurentpcaron@gmail.com>
2023-06-09 17:30:14 -0400

    Modal, debut

--- components/molecule/project.vue
+++ components/molecule/project.vue
@@ -1,14 +1,34 @@
 <template>
   <button
     class="w-full h-72 md:w-[calc(50%-1px)] lg:w-[calc((100%/3)-1px)] bg-mains"
+    @click="openModal()"
   >
     <div class="h-3/4"></div>
     <div class="h-1/4 bg-dark flex items-center justify-center">
       <p>Title</p>
     </div>
   </button>
+
+  <!--modal-->
+  <dialog class="absolute w-1/2" ref="modal" @click.self="closeModal">
+    <p>
+      {{ props.content }}
+    </p>
+  </dialog>
 </template>
 
-<script setup></script>
+<script lang="ts" setup>
+const props = defineProps(['content']);
+
+const modal = ref<HTMLDialogElement | null>(null);
+
+const openModal = () => {
+  modal.value?.showModal();
+};
+
+const closeModal = () => {
+  modal.value?.close();
+};
+</script>
 
 <style lang="scss" scoped></style>
--- components/organism/projects.vue
+++ components/organism/projects.vue
@@ -2,7 +2,7 @@
   <div>
     <h2 class="ml-5">Projets</h2>
     <div class="flex flex-wrap gap-[1px] bg-lighter border border-lighter">
-      <MoleculeProject v-for="x in 3" />
+      <MoleculeProject v-for="x in 3" :content="`boite${x}`" />
     </div>
   </div>
 </template>
