1ce3ecb9911383b20c776a269413bc66458f8bc0
laurent <laurentpcaron@gmail.com>
2023-07-09 16:49:49 -0400

    .

--- components/molecule/project.vue
+++ components/molecule/project.vue
@@ -2,16 +2,16 @@
   <div class="md:grid grid-cols-2 gap-5">
     <div class="relative overflow-hidden">
       <img class="object-contain w-full h-full" src="/img/gameboy.png" alt="" />
-      <!--Ration de l'imag est 17:15 -->
+      <!--Ratio de l'image est 17:15 -->
       <div class="absolute top-0 w-full h-full grid place-items-center -z-10">
         <img
-          class="aspect-[17/15] w-64 object-cover"
-          src="https://ceblog.s3.amazonaws.com/wp-content/uploads/2021/03/10114044/image2-8.png"
+          class="aspect-[17/15] w-[54%] object-cover object-top"
+          src="/img/site1.jpg"
           alt=""
         />
       </div>
     </div>
-    <div :class="index % 2 === 0 ? '-order-1' : ''">
+    <div class="flex flex-col" :class="index % 2 === 0 ? '-order-1' : ''">
       <h3>Titre du projet</h3>
       <p>
         Description du projet Lorem ipsum dolor, sit amet consectetur
@@ -21,9 +21,11 @@
       <div class="flex">
         <AtomIcons class="w-14" icon="tw" v-for="x in 3" />
       </div>
-      <div class="flex justify-around">
-        <AtomSimpleButton class="w-1/3">Voir en ligne</AtomSimpleButton>
-        <AtomSimpleButton class="w-1/3">Lire le code</AtomSimpleButton>
+      <div class="flex justify-around mt-16">
+        <AtomSimpleButton class="w-5/12 h-10"
+          >Voir en ligne<AtomIcons icon=""
+        /></AtomSimpleButton>
+        <AtomSimpleButton class="w-5/12 h-10">Lire le code</AtomSimpleButton>
       </div>
     </div>
   </div>
