eb3bccf2be71b1732f9a394ad599c6e8b3aa749b
LaurentPCaron <laurentpcaron@gmail.com>
2023-08-28 19:48:26 -0400

    contact

--- components/organism/about.vue
+++ components/organism/about.vue
@@ -3,23 +3,25 @@
     class="grid grid-rows-[10%_90%] gap-5 md:flex-row py-2 bg-[url('/img/about-bg.jpg')] bg-fixed"
   >
     <h2 class="self-start">Un peux à propos de moi</h2>
-    <div class="lg:w-1/2 mx-auto self-start">
-      <div class="mb-10 lg:flex items-center">
-        <p class="ml-5 self-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
-          necessitatibus earum amet laudantium velit enim hic iure. Inventore
-          necessitatibus suscipit provident quis velit, itaque adipisci
-          molestias aut hic ut non voluptate, modi cumque accusamus illum
-          laboriosam voluptates cupiditate sed architecto distinctio laudantium
-          culpa sint dolore. Aut facere ducimus qui est.
-        </p>
+    <div class="bg-mains bg-opacity-30 h-fit py-10">
+      <div class="lg:w-1/2 mx-auto self-start">
+        <div class="mb-10 lg:flex items-center">
+          <p class="ml-5 self-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repudiandae, necessitatibus earum amet laudantium velit enim hic
+            iure. Inventore necessitatibus suscipit provident quis velit, itaque
+            adipisci molestias aut hic ut non voluptate, modi cumque accusamus
+            illum laboriosam voluptates cupiditate sed architecto distinctio
+            laudantium culpa sint dolore. Aut facere ducimus qui est.
+          </p>
+        </div>
+        <h3>Mes skills</h3>
+        <ul class="flex gap-16 mx-auto">
+          <li v-for="s in skills" class="h-16">
+            <AtomIcons class="h-full" :icon="s" show-caption />
+          </li>
+        </ul>
       </div>
-      <h3>Mes skills</h3>
-      <ul class="flex gap-16 mx-auto">
-        <li v-for="s in skills" class="h-16">
-          <AtomIcons class="h-full" :icon="s" show-caption />
-        </li>
-      </ul>
     </div>
   </div>
 </template>
--- components/organism/contactme.vue
+++ components/organism/contactme.vue
@@ -1,17 +1,11 @@
 <template>
   <div>
     <h2>Me contacter</h2>
-    <div class="lg:w-1/2 mx-auto">
-      <p class="text-7xl text-center"><strong>Dites-moi bonjours!</strong></p>
-      <p>
-        Ma boîtes de couriel est toujours ouverte. Donc, n'hésitez pas à me
-        contacter que ce soit de nouvelle opportunité de carrière ou simplement
-        me dire allo. Je ferez mon possible pour vous répondre dans les bref
-        délais.
-      </p>
-      <AtomSimpleButton class="mx-auto mt-14 mb-40"
-        >Me dire allo!</AtomSimpleButton
-      >
-    </div>
+    <ul class="flex lg:w-1/2 mx-auto">
+      <li>Couriel</li>
+      <li>Linked</li>
+      <li>Github</li>
+      <li>C.V.</li>
+    </ul>
   </div>
 </template>
